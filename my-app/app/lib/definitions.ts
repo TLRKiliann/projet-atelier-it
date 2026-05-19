63929fc06d1ad1e8db2921f04e0a53bde6aaa151
TLRKiliann <philogenie@protonmail.com>
2026-05-19 14:06:32 +0200

    type space created

---
+++ my-app/app/lib/definitions.ts
@@ -0,0 +1,10 @@
+export type ItemsType = {
+    id: number;
+    stack_title: string;
+    item: string;
+    item_2: string;
+    item_3: string;
+    number_item_1: number; 
+    number_item_2: number;
+    number_item_3: number;
+}
