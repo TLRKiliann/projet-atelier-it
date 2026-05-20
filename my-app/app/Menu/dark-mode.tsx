ea55630af4c1ec28c5597ee19f0dbbcf72dceaed
TLRKiliann <philogenie@protonmail.com>
2026-05-20 20:50:23 +0200

    darkmode implementation

---
+++ my-app/app/Menu/dark-mode.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+import styles from "../styles/nav-menu.module.scss";
+
+export default function DarkMode() {
+
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState<boolean>(false);
+
+    useEffect(() => setMounted(true), []);
+
+    if (!mounted) return <span className="w-8 h-5 block" />;
+    
+    return (
+        <button
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            className={styles.btn_dark_mode}
+            aria-label="Toggle dark mode"
+        >
+            {theme === 'dark' ? '☀️' : '🌙'}
+        </button>
+    )
+}
