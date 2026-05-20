878cf7451618294db9a4184b473409c18257c0f5
TLRKiliann <philogenie@protonmail.com>
2026-05-20 08:45:55 +0200

    menu created

---
+++ my-app/app/Menu/nav-menu.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import styles from "../styles/nav-menu.module.scss";
+
+export default function NavMenu() {
+    return (
+        <nav className={styles.navmenu}>
+            <ul className={styles.ulmenu}>
+                <li className={styles.li}>
+                    <Link href="/bloc_1">
+                        Bloc 1
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/bloc_2">
+                        Bloc 2
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/bloc_3">
+                        Bloc 3
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/bloc_4">
+                        Bloc 4
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/bloc_5">
+                        Bloc 5
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/bloc_6">
+                        Bloc 6
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/bloc_7">
+                        Bloc 7
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/bloc_8">
+                        Bloc 8
+                    </Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/bloc_9">
+                        Bloc 9
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+// {}
