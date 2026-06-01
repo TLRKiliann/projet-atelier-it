d8f08055db3efd9d056a8ed9d7e3f2ef0c045f0a
TLRKiliann <philogenie@protonmail.com>
2026-06-01 21:38:01 +0200

    reusable comp arrow-left done

--- my-app/app/bloc_4/[id]/page.tsx
+++ my-app/app/bloc_4/[id]/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import type { BlocItem, CategorieItem, ModeleItem } from '@/lib/definitions';
-import { useRouter, useParams } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { useState, useEffect } from "react";
 import BtnHome from '@/app/components/btn-home';
+import ArrowLeft from '@/app/components/arrow-left';
 import EditModel from '@/app/components/edit-model';
 import AddCategory from '@/app/components/add-category';
 import ModelForm from '@/app/components/model-form';
-import { FaTrash, FaEdit, FaArrowLeft } from "react-icons/fa";
-import styles from "../../styles/bloc.module.scss";
+import { FaTrash, FaEdit } from "react-icons/fa";
+import styles from "@/app/styles/bloc.module.scss";
+
 export default function CategoriePage_4() {
 
-  const router = useRouter();
+  //const router = useRouter();
   const params = useParams();
   const categorieId = params.id as string || undefined;
   
@@ -180,9 +182,7 @@ export default function CategoriePage_4() {
     return (
       <div className={styles.page_bloc}>
         <div className={styles.titleAndBtn}>
-          <button onClick={() => router.push("/bloc_4")} className={styles.btn_return}>
-            <FaArrowLeft size={32} />
-          </button>
+          <ArrowLeft blocId={"/bloc_4"} />
           
           <h1>Chargement...</h1>
 
@@ -196,9 +196,7 @@ export default function CategoriePage_4() {
     return (
       <div className={styles.page_bloc}>
         <div className={styles.titleAndBtn}>
-          <button onClick={() => router.push("/bloc_4")} className={styles.btn_return}>
-            <FaArrowLeft size={32} />
-          </button>
+          <ArrowLeft blocId={"/bloc_4"} />
           
           <h1>Catégorie non trouvée</h1>
 
@@ -211,9 +209,7 @@ export default function CategoriePage_4() {
   return (
     <div className={styles.page_bloc}>
       <div className={styles.titleAndBtn}>
-        <button onClick={() => router.push("/bloc_4")} className={styles.btn_return}>
-          <FaArrowLeft size={32} />
-        </button>
+        <ArrowLeft blocId={"/bloc_4"} />
         
         <h1>{categorie.nom}</h1>
 
